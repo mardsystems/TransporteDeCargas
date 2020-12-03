9e34716d9739ea0430f1d8211b42115f62f3df8d
Marcelo Dias <mardsystems@gmail.com>
2020-12-03 13:22:55 -0300

    task: redefine namespace do projeto da Api

--- TransporteDeCargas.Api/Startup.cs
+++ TransporteDeCargas.Api/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TransporteDeCargas.Api
+namespace TransporteDeCargas
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace TransporteDeCargas.Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TransporteDeCargas.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TransporteDeCargas", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace TransporteDeCargas.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TransporteDeCargas.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TransporteDeCargas v1"));
             }
 
             app.UseHttpsRedirection();
